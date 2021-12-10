972477aec7a6bd485a9cca39ff0f443f2e45bc51
FunnyDevP <Waranchit.ch@outlook.com>
2021-12-10 09:43:03 +0700

    initial project

---
+++ clone_dev_to/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace clone_dev_to;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
